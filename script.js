c8587fc38660c798849a4b2d2ba864d972192262
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-03-06 16:26:10 +0100

    feat: add layout and styles for question page

---
+++ script.js
@@ -0,0 +1,3 @@
+const questionCount = document.querySelector(".question-count");
+const questionBody = document.querySelector(".question-body");
+const choices = document.querySelector(".option-text");
